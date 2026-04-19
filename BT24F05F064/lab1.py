6482f5c9f80151a5e3564baeda7c6a2372cae2ef
shraddhaujgare6-commits <shraddhaujgare6@gmail.com>
2026-04-19 14:03:46 +0530

    Add Python environment info script

---
+++ BT24F05F064/lab1.py
@@ -0,0 +1,11 @@
+//practical 1 : Installation of python 
+import sys
+import platform
+
+print("===== Python Environment Info =====")
+print("Python Version:", sys.version)
+print("Python Path   :", sys.executable)
+print("Platform      :", platform.system(), platform.release())
+print("Architecture  :", platform.architecture()[0])
+print("")
+print("Installation verified successfully
